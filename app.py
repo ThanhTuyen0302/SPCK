3458c733f480fd160bc3f2127698d17b4a24229a
ThanhTuyen0302 <doancaothanhtuyen0302@gmail.com>
2025-11-23 10:50:12 +0700

    test

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
+from tkinter import Label
 import streamlit as st
+import os
 
+def menu():
+    st.sidebar.page_link("app.py", label="Home")
+    st.sidebar.page_link("page/1_phan_tich_du_lieu.py", label="Phân tích dữ liệu")
+    st.sidebar.page_link("page/2_them_du_lieu_test.py", label="Thêm dữ liệu cho tập test")
+    st.sidebar.page_link("page/3_du_doan_mo_hinh.py", label="Phân tích dự đoán")
+    
+if __name__ == "__main__":
+    st.set_page_config(
+        page_title="",  # tên sản phẩm
+        layout="centered",
+        page_icon="👋",
+    )
 
-st.title("Hello world")
+    st.title("")  # tên sản phẩm
+    st.header("Chức năng")
+    st.markdown(
+        """
+    1. Xem phân tích tập dữ liệu ...
+    2. Thêm dữ liệu mới và cập nhật các biểu đồ
+    3. Sử dụng AI để dự đoán ...
+    """
+    )
+
+    st.subheader("Credits")
+    st.markdown(
+        """
+        Ứng dựng được xây dựng với [streamlit](https://streamlit.io) và [Plotly](https://plotly.com/).
+        
+        Được phát triển bởi [Ngọc Diệp](link github)
+        """
+    )
+
+    menu()
